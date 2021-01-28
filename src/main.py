0fa8405479cc75e16f9cc98ae3ef88939399dbb7
Taylor Turner <taylorfturner@gmail.com>
2021-01-28 18:28:08 -0500

    update main.py with prams

--- src/main.py
+++ src/main.py
@@ -9,10 +9,12 @@ rr = RescaledRange()
 with Flow('rescaled_range') as flow:
 
     data_type = Parameter('data_type', default='dask')
+    file_path = Parameter('file_path', default='spy.csv')
     flow.add_task(data_type)
+    flow.add_task(file_path)
 
-    data = reader(data_type)
+    data = reader(data_type, file_path)
     rr_data = rr(data)
 
 flow.visualize()
-flow.run(data_type='dask')
+flow.run(data_type='dask', file_path='spy.csv')
