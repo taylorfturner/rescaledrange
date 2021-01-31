8362163453ab3fd916912e2aeb60391a99fdcc8c
Taylor Turner <taylorfturner@gmail.com>
2021-01-31 11:33:04 -0500

    update fixtures and test

--- tests/fixtures/data_fixtures.py
+++
@@ -1,6 +0,0 @@
-import pytest 
-import pandas as pd
-
-@pytest.fixture('data_fixture')
-def data_fixture():
-    return pd.DataFrame('../../rescaledranges/data/{}.{}')
--- tests/tasks/test_read_data_tasks.py
+++ tests/tasks/test_read_data_tasks.py
@@ -1,32 +1,34 @@
 import pytest
 from prefect import Task
 from rescaledranges.tasks.read_data_tasks import DataReader
-from fixtures.data_fixtures import data_fixture
+from data_fixtures import data_fixture
 
-def test_data_reader_no_params():
-    data_reader = DataReader()
-    assert data_reader
 
-def test_data_reader_failure_data_frame_type():
-    with pytest.raises(ValueError):
+class TestReadData:
+    def test_data_reader_no_params():
         data_reader = DataReader()
-        data_reader.run(
-            data_frame_type='fail',
-            data_type='csv',
-            ticker='one')
+        assert data_reader
 
-def test_data_reader_pandas_run():
-    with pytest.raises(FileNotFoundError):
-        data_reader = DataReader()
-        data_reader.run(
-            data_frame_type='pandas',
-            data_type='csv',
-            ticker='one')
+    def test_data_reader_failure_data_frame_type():
+        with pytest.raises(ValueError):
+            data_reader = DataReader()
+            data_reader.run(
+                data_frame_type='fail',
+                data_type='csv',
+                ticker='one')
 
-def test_data_reader_file_not_found_error():
-    with pytest.raises(FileNotFoundError):
-        data_reader = DataReader()
-        data_reader.run(
-            data_frame_type='dask',
-            data_type='csv',
-            ticker='one')
+    def test_data_reader_pandas_run():
+        with pytest.raises(FileNotFoundError):
+            data_reader = DataReader()
+            data_reader.run(
+                data_frame_type='pandas',
+                data_type='csv',
+                ticker='one')
+
+    def test_data_reader_file_not_found_error():
+        with pytest.raises(FileNotFoundError):
+            data_reader = DataReader()
+            data_reader.run(
+                data_frame_type='dask',
+                data_type='csv',
+                ticker='one')
