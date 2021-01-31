43d50d5da8b6071dff74ebe6477d094027e52c4b
Taylor Turner <taylorfturner@gmail.com>
2021-01-30 22:18:51 -0500

    update read_data_test

--- tests/tasks/test_read_data_tasks.py
+++ tests/tasks/test_read_data_tasks.py
@@ -4,8 +4,25 @@ from rescaledranges.tasks.read_data_tasks import DataReader
 
 
 def test_data_reader_no_params():
-    assert DataReader()
-
+    data_reader = DataReader()
+    assert data_reader
+    
 def test_data_reader_type():
     data_reader = DataReader()
     assert type(data_reader) == type(Task)
+
+def test_data_reader_failure_data_frame_type():
+    with pytest.raises(ValueError):
+        data_reader = DataReader()
+        data_reader.run(
+            data_frame_type='fail',
+            data_type='csv',
+            ticker='one')
+
+def test_data_reader_file_not_found_error():
+    with pytest.raises(FileNotFoundError):
+        data_reader = DataReader()
+        data_reader.run(
+            data_frame_type='pandas',
+            data_type='csv',
+            ticker='one')
