cb037bb9c89e1d0405642df1382ebdb6d6256cf2
Taylor Turner <taylorfturner@gmail.com>
2021-01-31 08:03:43 -0500

    update `.gitignore`

--- rescaledranges/tests/tasks/test_read_data_tasks.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from rescaledranges.tasks.read_data_tasks import DataReader
-
-
-def test_data_reader_no_params():
-    assert DataReader()
--- tests/tasks/test_read_data_tasks.py
+++ tests/tasks/test_read_data_tasks.py
@@ -2,14 +2,9 @@ import pytest
 from prefect import Task
 from rescaledranges.tasks.read_data_tasks import DataReader
 
-
 def test_data_reader_no_params():
     data_reader = DataReader()
     assert data_reader
-    
-def test_data_reader_type():
-    data_reader = DataReader()
-    assert type(data_reader) == type(Task)
 
 def test_data_reader_failure_data_frame_type():
     with pytest.raises(ValueError):
@@ -26,3 +21,7 @@ def test_data_reader_file_not_found_error():
             data_frame_type='pandas',
             data_type='csv',
             ticker='one')
+
+def test_data_reader_type():
+    data_reader = DataReader()
+    assert type(data_reader) == type(Task)
