ea16fc41bb23c4e0008707bb47afa52f8920173a
Taylor Turner <taylorfturner@gmail.com>
2021-02-16 20:30:05 -0500

    update white space

--- rescaledranges/tasks/metrics_tasks.py
+++ rescaledranges/tasks/metrics_tasks.py
@@ -4,8 +4,8 @@ from prefect import Task
 class Metrics(Task):
     def __init__(self):
         """
-        Metrics Subclass of Prefect Task class. This should include 
-        everything from count of buy / sell signals, confusion matrix, 
+        Metrics Subclass of Prefect Task class. This should include
+        everything from count of buy / sell signals, confusion matrix,
         and any other metric related to the success of failure of
         the system on the particular ticker.
 
