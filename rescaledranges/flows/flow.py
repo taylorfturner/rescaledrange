a67db45fd6966c24f31c9e4fce52f4714533cab0
Taylor Turner <taylorfturner@gmail.com>
2021-02-16 20:29:02 -0500

    fix flow single to double quotes

--- rescaledranges/flows/flow.py
+++ rescaledranges/flows/flow.py
@@ -7,17 +7,17 @@ from tasks.read_data_tasks import DataReader
 
 
 class Flow(Flow):
-    def __init__(self, flow_name='rescaled_range'):
+    def __init__(self, flow_name="rescaled_range"):
         super().__init__(flow_name)
 
     @classmethod
-    def create_flow(cls, data_list=['data_one', 'data_two', 'data_three']):
+    def create_flow(cls, data_list=["data_one", "data_two", "data_three"]):
 
         reader = DataReader()
         rr = RescaledRange()
 
-        data_type = Parameter('data_type', default='pandas')
-        data_list = Parameter('data_list', default=['data_one', 'data_two', 'data_three'])
+        data_type = Parameter("data_type", default="pandas")
+        data_list = Parameter("data_list", default=["data_one", "data_two", "data_three"])
 
         cls.add_edge(upstream_task=data_type, downstream_task=reader)
         cls.add_edge(upstream_task=data_list, downstream_task=reader)
